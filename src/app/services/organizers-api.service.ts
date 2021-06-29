287e9d6135a8a66a6d1f44e33478a545ca7794ef
by-German <german280599@gmail.com>
2021-06-29 16:55:30 -0500

    feat: add cors origin

--- src/app/services/organizers-api.service.ts
+++ src/app/services/organizers-api.service.ts
@@ -7,7 +7,7 @@ import { Observable } from 'rxjs';
   providedIn: 'root'
 })
 export class OrganizersApiService {
-  basePath = "http://localhost:3000";
+  basePath = "http://localhost:8080/api";
 
   constructor(private http: HttpClient) { }
 
