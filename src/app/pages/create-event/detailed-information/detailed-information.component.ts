dbb9a4b409f3796ad15ea765c1e9ceaacd30a95b
by-German <german280599@gmail.com>
2021-09-12 00:09:19 -0500

    feat: add image for event information

--- src/app/pages/create-event/detailed-information/detailed-information.component.ts
+++ src/app/pages/create-event/detailed-information/detailed-information.component.ts
@@ -5,6 +5,7 @@ import {AuthService} from "../../../services/auth.service";
 import {TokenStorageService} from "../../../services/token-storage.service";
 import {EventItinerariesApiService} from "../../../services/event/event-itineraries-api.service";
 import {EventsInformationApiService} from "../../../services/event/events-information-api.service";
+import {CloudinaryApiService} from "../../../services/cloudinary-api.service";
 
 @Component({
   selector: 'app-detailed-information',
@@ -26,15 +27,18 @@ export class DetailedInformationComponent implements OnInit {
   countInf : number = 0;
   information = new FormGroup({
     title1: new FormControl('', [Validators.required]),
-    description1: new FormControl('', [Validators.required]),
-    image1: new FormControl('')
+    description1: new FormControl('', [Validators.required])
   })
+  pathImg : any[] = []
+  files : any[] = [];
+
 
   constructor(
     private router: Router,
     private route: ActivatedRoute,
     private itinerariesService: EventItinerariesApiService,
     private informationService: EventsInformationApiService,
+    private cloudinary: CloudinaryApiService,
     private authService: AuthService,
     private tokenStorageService: TokenStorageService) {
   }
@@ -73,19 +77,35 @@ export class DetailedInformationComponent implements OnInit {
     this.listInformation.push(++this.countInf);
     this.information.addControl("title" + this.countInf, new FormControl('', [Validators.required]))
     this.information.addControl("description" + this.countInf, new FormControl('', [Validators.required]))
-    this.information.addControl("image" + this.countInf, new FormControl(''))
   }
   onSubmitInformation(): void {
     if (this.information.invalid) {
       return;
     }
     for (let i = 1; i < this.countInf + 1; i++) {
-      this.informationService.postEventInformation(this.eventId, {
-        title: "" + this.information.value[`title${i}`],
-        description: "" + this.information.value[`description${i}`],
-        image: "" + this.information.value[`image${i}`]
-      }).subscribe(result => console.log(result))
+      // generate link img
+      let ref = this.cloudinary.reference(this.files[i - 1].name)
+      this.cloudinary.post(this.files[i-1]).then(result => {
+        ref.getDownloadURL().subscribe((url: any) => {
+          // creation of event information
+          this.informationService.postEventInformation(this.eventId, {
+            title: "" + this.information.value[`title${i}`],
+            description: "" + this.information.value[`description${i}`],
+            image: "" + url
+          }).subscribe(result => console.log(result))
+        })
+      })
     }
+  }
+  onChange() {
+    let img = document.getElementById('input-img');
+    // @ts-ignore
+    this.files.push(img.files[0])
+    // @ts-ignore
+    let path = URL.createObjectURL(img.files[0]);
+    this.pathImg.push(path)
+
+    console.log(this.files)
   }
 
 
